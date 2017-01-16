46126f670ea941d57d43cb7ea66ce9466e8aa86b
Omar Aguinaga <omar.aguinaga94@gmail.com>
2017-01-16 17:06:31 -0600

    A6: Add service Fixtures to collection

--- dist/scripts/services/Fixtures.js
+++ dist/scripts/services/Fixtures.js
@@ -37,6 +37,15 @@
             return albumPicasso;
         };
         
+        Fixtures.getCollection = function(numberOfAlbums){
+            var albums = [];
+            for (var i=0; i < numberOfAlbums; i++) {
+                albums.push(albumPicasso);
+            }
+            
+            return albums;
+        };
+        
         return Fixtures;
     }
     
