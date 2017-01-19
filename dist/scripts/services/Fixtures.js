4ed01a918feca06d4735d1ae423740eb5e5f455d
Omar Aguinaga <omar.aguinaga94@gmail.com>
2017-01-19 10:56:07 -0600

    CP7: Add Play and Pause functionality to service

--- dist/scripts/services/Fixtures.js
+++ dist/scripts/services/Fixtures.js
@@ -7,13 +7,13 @@
             artist: 'Pablo Picasso',
             label: 'Cubism',
             year: '1881',
-            albumArtUrl: '/assets/images/album_covers/01.png',
+            albumArtUrl: 'assets/images/album_covers/01.png',
             songs: [
-                { title: 'Blue', duration: '4:26' },
-                { title: 'Green', duration: '3:14' },
-                { title: 'Red', duration: '5:01' },
-                { title: 'Pink', duration: '3:21'},
-                { title: 'Magenta', duration: '2:15'}
+                { title: 'Blue', duration: 161.71, audioUrl: '/assets/music/blue' },
+                { title: 'Green', duration: 103.96, audioUrl: '/assets/music/green' },
+                { title: 'Red', duration: 268.45, audioUrl: '/assets/music/red' },
+                { title: 'Pink', duration: 153.14, audioUrl: '/assets/music/pink' },
+                { title: 'Magenta', duration: 374.22, audioUrl: '/assets/music/magenta' }
             ]
         };
 
@@ -23,7 +23,7 @@
             artist: 'Guglielmo Marconi',
             label: 'EM',
             year: '1909',
-            albumArtUrl: '/assets/images/album_covers/20.png',
+            albumArtUrl: 'assets/images/album_covers/20.png',
             songs: [
                 { title: 'Hello, Operator?', duration: '1:01' },
                 { title: 'Ring, ring, ring', duration: '5:01' },
@@ -32,6 +32,7 @@
                 { title: 'Wrong phone number', duration: '2:15'}
             ]
         };
+
 
         Fixtures.getAlbum = function(){
             return albumPicasso;
